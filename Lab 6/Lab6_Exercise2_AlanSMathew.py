c2e1aa287e42a89fc108df7ff812926d3d45c7b6
Alan S Mathew <alansmathewibab@gmail.com>
2025-08-13 14:11:30 +0530

    Add files via upload

---
+++ Lab 6/Lab6_Exercise2_AlanSMathew.py
@@ -0,0 +1,27 @@
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+plt.ion()   # interactive mode
+
+
+
+landmarks_frame = pd.read_csv('data/faces/face_landmarks.csv')
+
+n = 65
+img_name = landmarks_frame.iloc[n, 0]
+landmarks = landmarks_frame.iloc[n, 1:]
+landmarks = np.asarray(landmarks, dtype=float).reshape(-1, 2)
+
+print('Image name: {}'.format(img_name))
+print('Landmarks shape: {}'.format(landmarks.shape))
+print('First 4 Landmarks: {}'.format(landmarks[:4]))
